1c9cd7bd37f6a05b48a3e988c77ac0c295b437f7
Abyan Falah <abyanf410@gmail.com>
2023-03-23 16:08:53 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,7 @@ router.get("/", (req, res) => {
 	res.send("hello world");
 });
 
-router.get("/test", async (req, res) => {
-	const rentId = "c4d23043-8aff-4f72-9c3b-8a90367fd89b";
-	let data = (await rentDetailModel.getByRentId(rentId))[0];
-	const gerobakId = data.gerobak_id;
-	data.sub_amount = await rentModel.getRentDetailSubAmount(rentId, gerobakId);
-
-	res.send({ data });
-});
+router.get("/test", async (req, res) => {});
 
 router.get("/session", (req, res) => {
 	res.send(req.session);
