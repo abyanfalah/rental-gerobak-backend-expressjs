26731688281ae65223d41d406f9e3078fc4ed7a3
Abyan Falah <abyanf410@gmail.com>
2023-03-24 14:32:50 +0700

    Revert "Update is-identical-object.js"
    This reverts commit 99936c0f1dcc93111805c608f9f77382518b64c3.

--- helper/is-identical-object.js
+++ helper/is-identical-object.js
@@ -1,3 +1,10 @@
 module.exports = (obj1, obj2) => {
-	return JSON.stringify(obj1) === JSON.stringify(obj2);
+	let result = true;
+	for (let prop in obj1) {
+		if (obj1[prop] != obj2[prop]) {
+			result = false;
+			break;
+		}
+	}
+	return result;
 };
